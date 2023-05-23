05636f1627c41a85355eee1d49bff05b183c845e
NalonOff <nalon.projects@gmail.com>
2023-05-23 22:47:18 +0200

    Add the map in .tmx

---
+++ assets/Map/InteriorWallSimple.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="InteriorWallSimple" tilewidth="16" tileheight="16" tilecount="110" columns="10">
+ <image source="../Backgrounds/Tilesets/Interior/TilesetWallSimple.png" trans="000000" width="160" height="176"/>
+</tileset>
---
+++ assets/Map/Water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Water" tilewidth="16" tileheight="16" tilecount="476" columns="28">
+ <image source="../Backgrounds/Tilesets/TilesetWater.png" trans="000000" width="448" height="272"/>
+</tileset>
